e25d0eb14f8251aaa063c7820add9cc3bb63b4cc
Corbacho, Jose A <JoseAntonio.Corbacho@sky.uk>
2016-03-14 16:13:32 +0000

    Add some comments

--- datasource/src/main/java/com/mccorby/reactivegithubusers/datasource/NetworkDatasourceImpl.java
+++ datasource/src/main/java/com/mccorby/reactivegithubusers/datasource/NetworkDatasourceImpl.java
@@ -32,26 +32,32 @@ public class NetworkDatasourceImpl implements UserDatasource {
         final ApiMapper mapper = new ApiMapper();
         int randomOffset = (int) Math.floor(Math.random()*500);
         Observable<List<GitHubUser>> listObservable = api.getUsers(randomOffset)
+                // Uses a cache
                 .cache()
+                // Transform the observer with a list into a list of observers
                 .flatMap(new Func1<List<ApiSummaryUser>, Observable<ApiSummaryUser>>() {
                     @Override
                     public Observable<ApiSummaryUser> call(List<ApiSummaryUser> apiSummaryUsers) {
                         return Observable.from(apiSummaryUsers);
                     }
                 })
+                // To each obversable in the stream of observable, applies mapper.toGitHubUser
                 .map(new Func1<ApiSummaryUser, GitHubUser>() {
                     @Override
                     public GitHubUser call(ApiSummaryUser apiSummaryUser) {
                         return mapper.toGitHubUser(apiSummaryUser);
                     }
                 })
+                // Limit the stream to 3 elements
                 .limit(limit)
+                // Transform the stream into an observable of a list
                 .toList();
         return listObservable;
     }
 
     @Override
     public Observable<GitHubUser> user(String id) {
+        // TBD
         api.getUserDetails(id);
         return null;
     }
